1d69e2589f0647cac767e313dc55dceefd74ee61
TPei <t.s.peikert@gmail.com>
2016-05-26 12:36:55 +0200

    adds error_watcher to be used with circuit_breaker soon

---
+++ spec/error_watcher_spec.cr
@@ -0,0 +1,33 @@
+require "spec"
+require "../src/error_watcher.cr"
+
+describe "ErrorWatcher" do
+  describe "#add_failure" do
+    it "adds a failure timestamp to @failures array" do
+    end
+  end
+
+  describe "#error_rate" do
+    it "calculates error rate correctly and cleans after time" do
+      watcher = ErrorWatcher.new(Time::Span.new(0, 0, 1))
+      watcher.add_failure
+      watcher.add_execution
+      watcher.error_rate.should eq 100
+      sleep 2
+      watcher.error_rate.should eq 0
+    end
+
+    it "throws an error if there are more failures than executions" do
+      watcher = ErrorWatcher.new(Time::Span.new(0, 0, 60))
+      watcher.add_failure
+      expect_raises InvalidStateException do
+        watcher.error_rate
+      end
+    end
+
+    it "returns 0 if failures and executions are empty" do
+      watcher = ErrorWatcher.new(Time::Span.new(0, 0, 60))
+      watcher.error_rate.should eq 0
+    end
+  end
+end
---
+++ src/error_watcher.cr
@@ -0,0 +1,57 @@
+class ErrorWatcher
+  @failures = [] of Time
+  @executions = [] of Time
+  @timeframe : Time::Span
+
+  def initialize(@timeframe)
+  end
+  
+  def add_failure
+    @failures << Time.new
+  end
+
+  def add_execution
+    @executions << Time.new
+  end
+
+  def reset
+    @failures = [] of Time
+    @executions = [] of Time
+  end
+  
+  def error_rate : Float64
+    clean_old_records
+
+    raise InvalidStateException.new if failure_count > execution_count
+    return 0.to_f if @executions.empty?
+
+    @failures.size / @executions.size.to_f * 100
+  end
+
+  # ---------------------------
+  # private methods 
+  # ---------------------------
+  
+  private def clean_old_records
+    clean_old @failures
+    clean_old @executions
+  end
+
+  private def clean_old(arr : Array(Time))
+    threshold = Time.new - @timeframe
+
+    arr.reject! { |time| time < threshold }
+  end
+
+  private def failure_count
+    @failures.size
+  end
+
+  private def execution_count
+    @executions.size
+  end
+
+end
+
+class InvalidStateException < Exception
+end
