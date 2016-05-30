d0bc6659c94d18a6753314c83256a0b45cd5567e
TPei <t.s.peikert@gmail.com>
2016-05-30 15:44:35 +0200

    refactors and extends specs

--- spec/circuit_breaker_spec.cr
+++ spec/circuit_breaker_spec.cr
@@ -2,26 +2,70 @@ require "spec"
 require "../src/circuit_breaker.cr"
 
 describe "CircuitBreaker" do
+  describe "#run" do
+    it "returns original block value on success" do
+      breaker = CircuitBreaker.new(threshold: 20, timewindow: 60, reenable_after: 2)
+
+      breaker.run do
+        2
+      end.should eq 2
+    end
+
+    it "passes on any raised exceptions" do
+      breaker = CircuitBreaker.new(threshold: 20, timewindow: 60, reenable_after: 2)
+
+      expect_raises MyError do
+        breaker.run do
+          raise MyError.new
+        end
+      end
+    end
+
+    it "throws a CircuitOpenException if the circuit is open" do
+      breaker = CircuitBreaker.new(threshold: 20, timewindow: 60, reenable_after: 2)
+
+      expect_raises MyError do
+        breaker.run do
+          raise MyError.new
+        end
+      end
+
+      expect_raises CircuitOpenException do
+        breaker.run do
+          2
+        end
+      end
+
+      expect_raises CircuitOpenException do
+        breaker.run do
+          raise MyError.new
+        end
+      end
+    end
+  end
+
   describe "feature test" do
     it "reenables after a given timeframe" do
       breaker = CircuitBreaker.new(threshold: 20, timewindow: 60, reenable_after: 2)
 
-      (1..20).each do |n|
-        if n <= 10
+      10.times do
+        breaker.run do
+          "swag"
+        end.should eq "swag"
+      end
+
+      3.times do
+        expect_raises ArgumentError do
+          breaker.run do
+            raise ArgumentError.new
+          end
+        end
+      end
+
+      7.times do
+        expect_raises CircuitOpenException do
           breaker.run do
             "swag"
-          end.should eq "swag"
-        elsif n > 10 && n < 14
-          expect_raises ArgumentError do
-            breaker.run do
-              raise ArgumentError.new
-            end
-          end
-        else
-          expect_raises CircuitOpenException do
-            breaker.run do
-              "swag"
-            end
           end
         end
       end
@@ -36,22 +80,24 @@ describe "CircuitBreaker" do
     it "goes directly back to open if the first execution after reopening fails" do
       breaker = CircuitBreaker.new(threshold: 20, timewindow: 60, reenable_after: 2)
 
-      (1..20).each do |n|
-        if n <= 10
+      10.times do
+        breaker.run do
+          "swag"
+        end.should eq "swag"
+      end
+
+      3.times do
+        expect_raises ArgumentError do
+          breaker.run do
+            raise ArgumentError.new
+          end
+        end
+      end
+
+      7.times do
+        expect_raises CircuitOpenException do
           breaker.run do
             "swag"
-          end.should eq "swag"
-        elsif n > 10 && n < 14
-          expect_raises ArgumentError do
-            breaker.run do
-              raise ArgumentError.new
-            end
-          end
-        else
-          expect_raises CircuitOpenException do
-            breaker.run do
-              "swag"
-            end
           end
         end
       end
@@ -74,41 +120,12 @@ describe "CircuitBreaker" do
     it "errors and executions only count in a given timeframe" do
       breaker = CircuitBreaker.new(threshold: 20, timewindow: 2, reenable_after: 60)
 
-      (1..12).each do |n|
-        if n <= 10
-          breaker.run do
-            "swag"
-          end.should eq "swag"
-        else
-          expect_raises ArgumentError do
-            breaker.run do
-              raise ArgumentError.new
-            end
-          end
-        end
+      10.times do
+        breaker.run do
+          "swag"
+        end.should eq "swag"
       end
-
-      sleep 3
-
-      (1..5).each do |n|
-        if n <= 4
-          breaker.run do
-            "swag"
-          end.should eq "swag"
-        else
-          expect_raises ArgumentError do
-            breaker.run do
-              raise ArgumentError.new
-            end
-          end
-        end
-      end
-    end
-
-    it "if the breaker was given an array of Exception types, only those will be monitored" do
-      breaker = CircuitBreaker.new(threshold: 20, timewindow: 2, reenable_after: 60, handled_errors: [MyError.new])
-
-      (1..10).each do |n|
+      2.times do
         expect_raises ArgumentError do
           breaker.run do
             raise ArgumentError.new
@@ -116,7 +133,33 @@ describe "CircuitBreaker" do
         end
       end
 
-      (1..3).each do |n|
+      sleep 3
+
+      4.times do
+        breaker.run do
+          "swag"
+        end.should eq "swag"
+      end
+
+      expect_raises ArgumentError do
+        breaker.run do
+          raise ArgumentError.new
+        end
+      end
+    end
+
+    it "if the breaker was given an array of Exception types, only those will be monitored" do
+      breaker = CircuitBreaker.new(threshold: 20, timewindow: 2, reenable_after: 60, handled_errors: [MyError.new])
+
+      10.times do
+        expect_raises ArgumentError do
+          breaker.run do
+            raise ArgumentError.new
+          end
+        end
+      end
+
+      3.times do
         expect_raises MyError do
           breaker.run do
             raise MyError.new
@@ -134,7 +177,7 @@ describe "CircuitBreaker" do
     it "if the breaker was given an array of Exception types to ignore, those will not be monitored" do
       breaker = CircuitBreaker.new(threshold: 20, timewindow: 2, reenable_after: 60, ignored_errors: [ArgumentError.new])
 
-      (1..10).each do |n|
+      10.times do
         expect_raises ArgumentError do
           breaker.run do
             raise ArgumentError.new
@@ -142,7 +185,7 @@ describe "CircuitBreaker" do
         end
       end
 
-      (1..3).each do |n|
+      3.times do
         expect_raises MyError do
           breaker.run do
             raise MyError.new
