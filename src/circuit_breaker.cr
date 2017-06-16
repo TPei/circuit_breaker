289e2b4a30fa3eaa2bbaaccab3e47680b1b8818b
TPei <t.s.peikert@gmail.com>
2017-06-16 11:13:18 +0200

    add crystal doc documentation

--- src/circuit_breaker.cr
+++ src/circuit_breaker.cr
@@ -1,11 +1,25 @@
 require "./circuit_state"
 require "./error_watcher"
 
+# Simple Implementation of the circuit breaker pattern in Crystal.
+#
+# Given a certain error threshold, timeframe and timeout window, a breaker can be used to monitor criticial command executions. Circuit breakers are usually used to prevent unnecessary requests if a server ressource et al becomes unavailable. This protects the server from additional load and allows it to recover and relieves the client from requests that are doomed to fail.
+#
+# Wrap API calls inside a breaker, if the error rate in a given time frame surpasses a certain threshold, all subsequent calls will fail for a given duration.
 class CircuitBreaker
   @error_threshold : Int32
   @duration : Int32
   @reclose_time : Time
 
+  # creates a CircuitBreaker instance with a specified error threshold, timeframe, breaker duration and optionally a number of ignored or handled errors
+  #
+  # ```
+  # breaker = CircuitBreaker.new(
+  #   threshold: 5, # % of errors before you want to trip the circuit
+  #   timewindow: 60, # in s: anything older will be ignored in error_rate
+  #   reenable_after: 300 # after x seconds, the breaker will allow executions again
+  # )
+  # ```
   def initialize(threshold @error_threshold, timewindow timeframe, reenable_after @duration, handled_errors = [] of Exception, ignored_errors = [] of Exception)
     @state = CircuitState.new
     @reclose_time = Time.new
@@ -18,6 +32,20 @@ class CircuitBreaker
     @ignored_errors += ignored_errors
   end
 
+  # get's passed a block to watch for errors
+  # every error thrown inside your block counts towards the error rate
+  # once the threshold is surpassed, it starts throwing `CircuitOpenException`s
+  # you can catch these rrors and implement some fallback behaviour
+  # ```
+  # begin
+  #   breaker.run do
+  #     my_rest_call()
+  #   end
+  # rescue exc : CircuitOpenException
+  #   log "happens to the best of us..."
+  #   42
+  # end
+  # ```
   def run(&block)
     # if open and not reclosable -> fail
     if open?
